ebeee1cbb60db2e402e9bc2811e6a6de7806ecde
omni <na>
2012-10-30 23:34:30 +1030

    Corrected usage of starfield thanks Beeblerox, starfield works on cpp.

--- BulletManager1/source/Registry.hx
+++ BulletManager1/source/Registry.hx
@@ -1,22 +1,30 @@
 package ;
 
+import org.flixel.FlxSprite;
+import org.flixel.plugin.photonstorm.FlxSpecialFX;
 import org.flixel.FlxG;
 import org.flixel.plugin.photonstorm.fx.StarfieldFX;
 
 class Registry
 {
-    public static var stars:StarfieldFX;
+    public static var stars:FlxSprite;
     public static var player:Player;
     public static var bullets:BulletManager;
 
     //Setup the Registry Objects
     public static function init()
     {
-        //create the Starfield BG and set the star direction up
-        stars = new StarfieldFX();
-        stars.create(0,0,FlxG.width,FlxG.height,300,1,20);
-        stars.setStarSpeed ( 0, 1 );
+
+        if (FlxG.getPlugin(FlxSpecialFX) == null)
+        {
+            FlxG.addPlugin(new FlxSpecialFX());
+        }
         
+        var starfield = FlxSpecialFX.starfield();
+        
+        stars = starfield.create(0, 0, FlxG.width,FlxG.height, 300);
+        starfield.setStarSpeed ( 0, 1 );
+    
         //create the player ship
         player = new Player();
         
